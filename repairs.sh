47aeee055dc7fe97769aebb5c3b679f340386131
Brexit Moore <brexit@aagmotors.com>
2022-05-25 21:21:26 -0500

    repair inventory

---
+++ repairs.sh
@@ -0,0 +1,8 @@
+# this is the list of repaired vehicles
+echo toyota sienna was repaired today
+date
+echo on monday we have to set up deployment
+echo deployment can start now
+sleep5
+echo deployment successful
+
