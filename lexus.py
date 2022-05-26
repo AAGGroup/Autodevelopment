1b71c71a937137ba91d790f4bfc1cc196411b8f8
Brexit Moore <brexit@aagmotors.com>
2022-05-25 20:36:01 -0500

    version 1

---
+++ lexus.py
@@ -0,0 +1,6 @@
+# this is a python file 
+print 'we are going into the indepth of lexus'
+print 'but times are challenging now'
+print 'but we will try our best on this dat'
+date
+
---
+++ toyota.sh
@@ -0,0 +1,8 @@
+echo There are lot of toyota brands
+echo the brands are so great 
+echo we have the best of the camry
+echo we have sienna 
+echo and so on 
+echo till
+date
+echo God help Nigeria
